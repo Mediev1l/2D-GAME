5fde090f3f13f43fdace1df8ce694284ac8e57dd
Kacp3r3 <kacper.sawicki.it@gmail.com>
2019-02-26 12:57:27 +0100

    Idk

--- OpenGL/src/Shader.h
+++ OpenGL/src/Shader.h
@@ -1,8 +1,8 @@
 #ifndef SHADER_H
 #define SHADER_H
 
-#include <glad/glad.h> // include glad to get all the required OpenGL headers
-#include <glm/glm.hpp>
+#include <GL/glew.h> // include glad to get all the required OpenGL headers
+#include <glm.hpp>
 
 #include <string>
 #include <fstream>
