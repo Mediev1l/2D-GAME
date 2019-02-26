16eb5093cd361bbd3ddaf468cb51a9fec9c5fae2
Kacp3r3 <kacper.sawicki.it@gmail.com>
2019-02-26 14:01:32 +0100

    Mam nadzieje ze dziala

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -117,7 +117,20 @@ static int CreateShader(const std::string& vertexShader, const std::string& frag
 
 }
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 1280;
+const unsigned int SCR_HEIGHT = 720;
+
+// camera
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
 
 Timer t;
 int main(void)
@@ -133,7 +146,7 @@ int main(void)
 	/* Initialize the library */
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Hello World", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -169,16 +182,24 @@ int main(void)
 		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,  // top left 
 	};
 
+	unsigned int indices[] = {  // note that we start from 0!
+	0, 1, 3,  // first Triangle
+	1, 2, 3   // second Triangle
+	};
+
+
 	Shader myShader("src/vs.vs", "src/fs.fs");
 
-	unsigned int VBO, VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-
+	glGenBuffers(1, &EBO);
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// position attribute
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
@@ -186,6 +207,9 @@ int main(void)
 	// texture coord attribute
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
 
 	
 
@@ -238,8 +262,8 @@ int main(void)
 		color += increment;
 
 		//GLLogCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
-		
-		glDrawArrays(GL_TRIANGLES, 0, 4);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 
 		/* Swap front and back buffers */
@@ -260,17 +284,59 @@ void processInput(GLFWwindow *window)
 		glfwSetWindowShouldClose(window, true);
 
 	float deltaTime = t.getGlobalDelta();
+
+
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		std::cout << "Klikam cos?";
-		//camera.ProcessKeyboard(FORWARD, deltaTime);
+	{
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	}
 	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		//camera.ProcessKeyboard(BACKWARD, deltaTime);
+	{ }
 	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		//camera.ProcessKeyboard(LEFT, deltaTime);
+	{
+	}
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		//camera.ProcessKeyboard(RIGHT, deltaTime);
+	{
+	}
 	if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	//camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	//camera.ProcessMouseScroll(yoffset);
 }
