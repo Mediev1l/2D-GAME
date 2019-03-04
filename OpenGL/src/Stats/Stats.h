a4a4f5f42c4aace1ebb57db1137560a11b14c3fb
Mediev1l <bartek5903@gmail.com>
2019-03-04 15:00:07 +0100

    Basics for piFpaF (no smoke)

--- OpenGL/src/Stats/Stats.h
+++ OpenGL/src/Stats/Stats.h
@@ -7,21 +7,28 @@ Klasa ze wszystkimi statystykami gracza jak i przeciwnikow
 #ifndef _Stats_H
 #define _Stats_H
 
+#include "GL/glew.h"
+
+
 class Stats
 {
 protected:
-	int m_health;
-	int m_shield; // albo tylko hp
+	GLuint m_health;
+	GLuint m_shield; // albo tylko hp
+	GLuint m_range;
 	double m_speed;
 	double m_att_speed;
 	double m_damage;
+
+
 public:
 	Stats();	
-	int getHealth()const { return m_health; };
-	int getShield()const { return m_shield; };
-	double getAttackSpeed()const { return m_att_speed; };
-	double getMovementSpeed()const { return m_speed; };
-	double getDamage()const { return m_damage; };
+	GLuint getHealth() const { return m_health; };
+	GLuint getShield() const { return m_shield; };
+	GLuint getRange() const { return m_range; };
+	double getAttackSpeed() const { return m_att_speed; };
+	double getMovementSpeed() const { return m_speed; };
+	double getDamage() const { return m_damage; };
 
 };
 
